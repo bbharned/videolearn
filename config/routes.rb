b1bbb33cc9e4b97e0efd88ea1970ec0c719aabb8
Bryan Harned <bbharned@gmail.com>
2016-10-26 15:06:30 -0400

    added styled messages for errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'pages#index'
+get 'signup', to: 'users#new'
+
 get 'dashboard', to: 'pages#dashboard'
 get 'lessonone', to: 'pages#lessonone'
 
