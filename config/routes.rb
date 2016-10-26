a3d21627372c3492b1dc5f5f116c861727025508
Bryan Harned <bbharned@gmail.com>
2016-10-26 18:06:49 -0400

    added user sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'pages#index'
+
 get 'signup', to: 'users#new'
+resources :users, except: [:new]
 
 get 'dashboard', to: 'pages#dashboard'
 get 'lessonone', to: 'pages#lessonone'
