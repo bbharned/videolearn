73e71269e748ff54dd9dab81627ffecc644f0afb
Bryan Harned <bbharned@gmail.com>
2016-10-27 15:33:16 -0400

    added sign up and user edit process with validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,27 +9,34 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-
-            flash[:success] = "Welcome to the alpha blog #{@user.username}"
-
-            redirect_to articles_path
-
+            flash[:success] = "Welcome to ThinManager Video Learning #{@user.firstname}"
+            redirect_to dashboard_path
         else
-
-        render 'new'
-
+            render 'new'
         end
 
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
 
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Account information was successfully updated"
+            redirect_to dashboard_path
+        else
+            render 'edit'
+        end
+    end
 
 
 
     private
 
     def user_params
-        params.require(:user).permit(:firstname, :lastname, :email, :company, :password)
+        params.require(:user).permit(:firstname, :lastname, :email, :email_confirmation, :company, :password, :password_confirmation)
     end
 
    
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ActiveRecord::Base
+    #attr_accessor :password_confirmation, :email_confirmation 
     validates :firstname, presence: true, length: { minimum: 1, maximum: 20 }
     validates :lastname, presence: true, length: { minimum: 3, maximum: 20 }
+    validates :password, confirmation: true, :on => :create
+    validates :email, confirmation: true, :on => :create
+    validates :email_confirmation, presence: true, :on => :create
+    validates :password_confirmation, presence: true, :on => :create
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
-    validates :email, presence: true, length: { maximum: 105 },
+    validates :email,  presence: true,  length: { maximum: 105 },
                 uniqueness: { case_sensitive: false },
                 format: { with: VALID_EMAIL_REGEX }
+
     has_secure_password
+
 end
