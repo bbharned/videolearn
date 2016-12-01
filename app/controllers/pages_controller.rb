89daa36cf02fb1e9f0ff37ba73029cbea48bdb7b
Bryan Harned <bharned@thinmanager.com>
2016-12-01 13:22:20 -0500

    styling to dashboard page and some validations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,24 @@
 class PagesController < ApplicationController
+	#before_action :set_user, except: [:index]
 
     def index
         @user = User.new
+        redirect_to dashboard_path if logged_in?
     end
 
     def dashboard
-
+    	redirect_to root_path if !logged_in?
     end
 
     def lessonone
 
     end
+
+
+
+
+    private
+
+    
 
 end
