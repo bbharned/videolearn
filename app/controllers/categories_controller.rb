7e7ffb625f125a456e521858f5f3e6adf561100c
Bryan Harned <bharned@thinmanager.com>
2016-12-07 12:24:33 -0500

    added category edit feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,6 +21,20 @@ class CategoriesController < ApplicationController
 		end
 	end
 
+	def edit
+		@category = Category.find(params[:id])
+	end	
+
+	def update
+		@category = Category.find(params[:id])
+		if @category.update(category_params)
+			flash[:success] = "Category name was successfully updated"
+			redirect_to category_path(@category)
+		else
+			render 'edit'
+		end
+	end
+
 
 	def show
 		@category = Category.find(params[:id])
