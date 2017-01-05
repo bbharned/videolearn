663e1349194024af9c72dab0972ed67c894c800b
Bryan Harned <bharned@thinmanager.com>
2017-01-05 12:43:33 -0500

    working on badge functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,50 @@ class PagesController < ApplicationController
     	@user = User.find(current_user.id)
         # put in badge detection here.
         @badge = UserBadge.where(user_id: @user.id).take
+        
+        @prod_category = Category.where(name: "Productivity").take
+        @prod_videos = @prod_category.videos.all
+        
+        @vis_category = Category.where(name: "Visualization").take
+        @vis_videos = @vis_category.videos.all
+
+        @sec_category = Category.where(name: "Security").take
+        @sec_videos = @sec_category.videos.all
+        
+        @mob_category = Category.where(name: "Mobility").take
+        @mob_videos = @mob_category.videos.all
+
+        
+        #### Productivity Badge ####
+        @catprod_user_watch = 0
+        @catprod_user_quiz = 0
+        @prod_videos.each do |video|
+            @uvquery = UserVideo.where(user_id: @user.id, video_id: video.id)
+            if @uvquery != nil 
+                @catprod_user_watch += 1
+            end
+            video.quizzes.each do |quiz|
+                @uqquery = UserQuiz.where(user_id: @user.id, quiz_id: quiz.id)
+                    if @uqquery != nil
+                        @catprod_user_quiz += 1
+                    end
+                end
+        end
+        
+        if (@prod_videos.count == @catprod_user_watch) && (@catprod_user_quiz == @prod_videos.count) && @catprod_user_watch > 0
+            if (@badge != nil && !@badge.productivity) || (@badge == nil)
+                @newprodbadge = UserBadge.new(user_id:@user.id, productivity: true)
+               if @newprodbadge.save
+                    #when productivity badge is awarded
+                    flash[:success] = "You earned your PRODUCTIVITY badge!!!"
+                    redirect_to dashboard_path 
+               else
+
+               end
+            end 
+        end
+        
+
     end
 
     
