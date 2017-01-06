b6f8793a32c757935a621bce5f28468c36e84ba3
Bryan Harned <bharned@thinmanager.com>
2017-01-06 12:36:13 -0500

    working on the password mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.compile = false
 
 
-  config.action_mailer.default_url_options = { :host => "thinmanager.com" }
+  config.action_mailer.default_url_options = { :host => "stormy-river.herokuapp.com" }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
