d99590fa4b423c75122e7488b4116d0a921012e7
Bryan Harned <bharned@thinmanager.com>
2017-02-23 09:19:46 -0500

    added downcase validation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
     validates :email,  presence: true,  length: { maximum: 105 },
                 uniqueness: { case_sensitive: false },
                 format: { with: VALID_EMAIL_REGEX }
+    before_save { self.email = email.downcase }
     has_secure_password
 
 # Sets the password reset attributes.
