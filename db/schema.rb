be423fbc13207382a15b2130adda9e2e110323cf
Bryan <bbharned@gmail.com>
2020-10-02 14:01:10 -0400

    initiate active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828131755) do
+ActiveRecord::Schema.define(version: 2020_10_02_180029) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "answers", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "correct",    default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string "name"
+    t.boolean "correct", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "attendees", force: :cascade do |t|
-    t.string   "firstname"
-    t.string   "lastname"
-    t.string   "company"
-    t.string   "email"
-    t.string   "relationship"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "street"
-    t.string   "street2"
-    t.string   "city"
-    t.string   "state"
-    t.integer  "zip"
-    t.string   "phone"
-    t.string   "carrier"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "company"
+    t.string "email"
+    t.string "relationship"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "street"
+    t.string "street2"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.string "phone"
+    t.string "carrier"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "event_attendees", force: :cascade do |t|
-    t.integer  "attendee_id"
-    t.integer  "event_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.boolean  "checkedin",   default: false
-    t.string   "lastname"
+    t.integer "attendee_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "checkedin", default: false
+    t.string "lastname"
   end
 
   create_table "event_categories", force: :cascade do |t|
@@ -62,91 +83,91 @@ ActiveRecord::Schema.define(version: 20170828131755) do
   end
 
   create_table "eventcats", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "events", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
+    t.string "name"
+    t.text "description"
     t.datetime "time"
-    t.integer  "cost"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "capacity"
+    t.integer "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "capacity"
     t.datetime "endtime"
   end
 
   create_table "question_answers", force: :cascade do |t|
-    t.integer  "question_id"
-    t.integer  "answer_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "question_id"
+    t.integer "answer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "quiz_questions", force: :cascade do |t|
-    t.integer  "quiz_id"
-    t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "quiz_id"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "quizzes", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "user_badges", force: :cascade do |t|
     t.integer "user_id"
-    t.boolean "productivity",  default: false
+    t.boolean "productivity", default: false
     t.boolean "visualization", default: false
-    t.boolean "security",      default: false
-    t.boolean "mobility",      default: false
+    t.boolean "security", default: false
+    t.boolean "mobility", default: false
   end
 
   create_table "user_quizzes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "quiz_id"
+    t.integer "user_id"
+    t.integer "quiz_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "user_videos", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "video_id"
+    t.integer "user_id"
+    t.integer "video_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "firstname"
-    t.string   "lastname"
-    t.string   "company"
-    t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "password_digest"
-    t.string   "reset_digest"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "company"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.boolean  "admin",           default: false
+    t.boolean "admin", default: false
     t.datetime "lastlogin"
-    t.boolean  "evtadmin",        default: false
+    t.boolean "evtadmin", default: false
   end
 
   create_table "venues", force: :cascade do |t|
-    t.string   "name"
-    t.string   "street"
-    t.string   "city"
-    t.string   "state"
-    t.integer  "zipcode"
+    t.string "name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.integer "zipcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -157,15 +178,15 @@ ActiveRecord::Schema.define(version: 20170828131755) do
   end
 
   create_table "video_quizzes", force: :cascade do |t|
-    t.integer  "video_id"
-    t.integer  "quiz_id"
+    t.integer "video_id"
+    t.integer "quiz_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "videos", force: :cascade do |t|
-    t.string   "name"
-    t.string   "url"
+    t.string "name"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
